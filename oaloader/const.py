a3a7ccd1eabde7dbef9578bbef982a7d5c03023e
elonzh <elonzh@qq.com>
2021-08-09 17:53:21 +0800

    bump version

--- oaloader/const.py
+++ oaloader/const.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import click
 
-version = "0.1.0"
+version = "0.2.0"
 server = None  # Run on local machine.
 subkey = r"Software\Microsoft\Office\16.0\WEF\TrustedCatalogs"
 app_name = "office-addin-sideloader"
